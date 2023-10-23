70099e1cbe68e0c391736f8e02b1da4b6912b293
tfkhdyt <me@tfkhdyt.my.id>
2023-10-23 15:18:33 +0700

    feat(landing): add tentang kami section

---
+++ src/app/_components/landing-page/tentang-kami.tsx
@@ -0,0 +1,19 @@
+export function TentangKami() {
+  return (
+    <section className='-ml-6 bg-[url(/img/tentang_kami_bg.svg)] bg-cover bg-no-repeat py-48'>
+      <div className='container mx-auto space-y-12 font-poppins'>
+        <p className='text-center text-2xl font-bold text-[#77425A]'>
+          <span className='text-[#F48C06]'>Apa itu</span> Yukitanya?
+        </p>
+        <p className='text-lg leading-loose text-[#696984]'>
+          Yukitanya adalah sebuah website yang menghubungkan banyak siswa ke
+          dalam sebuah forum diskusi untuk menyelesaikan tugas sekolah secara
+          bersama. Website ini memungkinkan seorang siswa untuk menanyakan
+          sebuah pertanyaan, kemudian siswa lain dapat membantu dengan menjawab
+          pertanyaan tersebut. Apabila siswa telah puas dengan jawabannya, maka
+          para siswa dapat memberi rating kepada pertanyaan tersebut.
+        </p>
+      </div>
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import { Hero } from "./_components/landing-page/hero";
-import { MataPelajaran } from "./_components/landing-page/mata-pelajaran";
+import { Hero } from './_components/landing-page/hero';
+import { MataPelajaran } from './_components/landing-page/mata-pelajaran';
+import { TentangKami } from './_components/landing-page/tentang-kami';
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <MataPelajaran />
+      <TentangKami />
     </main>
   );
 }
