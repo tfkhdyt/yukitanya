a32e6382826699f812e343b501b72d81fc8b7ebb
tfkhdyt <me@tfkhdyt.my.id>
2023-10-23 14:36:05 +0700

    style(prettier): set single quote to true

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
+  singleQuote: true,
+  jsxSingleQuote: true,
 };
 
 export default config;
