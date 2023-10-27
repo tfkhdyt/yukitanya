34f41b8e4b03031830fdb3110a39f5d9839b32e8
tfkhdyt <me@tfkhdyt.my.id>
2023-10-27 13:46:38 +0700

    fix(landing/header/mobile): change "Masuk" button text color to black

--- src/app/_components/landing-page/header.tsx
+++ src/app/_components/landing-page/header.tsx
@@ -68,7 +68,7 @@ export function Header() {
               </SheetClose>
             ))}
             <div className='space-x-3'>
-              <button className='rounded-lg bg-[#F48C06] px-4 py-2 font-bold text-white shadow-md'>
+              <button className='rounded-lg bg-[#F48C06] px-4 py-2 font-bold text-black shadow-md'>
                 Masuk
               </button>
               <button className='rounded-lg bg-[#77425A] px-4 py-2 font-bold text-white shadow-md'>
