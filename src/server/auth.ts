7c4a136d8c58332debbafdb937945e1d1692733a
tfkhdyt <me@tfkhdyt.my.id>
2023-10-27 11:23:14 +0700

    style: sort import

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,14 +1,12 @@
-import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import {
-  getServerSession,
   type DefaultSession,
+  getServerSession,
   type NextAuthOptions,
-} from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+} from 'next-auth';
 
-import { env } from "@/env.mjs";
-import { db } from "@/server/db";
-import { mysqlTable } from "@/server/db/schema";
+import { db } from '@/server/db';
+import { mysqlTable } from '@/server/db/schema';
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -16,13 +14,13 @@ import { mysqlTable } from "@/server/db/schema";
  *
  * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
  */
-declare module "next-auth" {
+declare module 'next-auth' {
   interface Session extends DefaultSession {
     user: {
       id: string;
       // ...other properties
       // role: UserRole;
-    } & DefaultSession["user"];
+    } & DefaultSession['user'];
   }
 
   // interface User {
@@ -48,10 +46,10 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: DrizzleAdapter(db, mysqlTable),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
+    // DiscordProvider({
+    //   clientId: env.DISCORD_CLIENT_ID,
+    //   clientSecret: env.DISCORD_CLIENT_SECRET,
+    // }),
     /**
      * ...add more providers here.
      *
--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -1,12 +1,13 @@
-"use client";
+'use client';
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { loggerLink, unstable_httpBatchStreamLink } from "@trpc/client";
-import { createTRPCReact } from "@trpc/react-query";
-import { useState } from "react";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { loggerLink, unstable_httpBatchStreamLink } from '@trpc/client';
+import { createTRPCReact } from '@trpc/react-query';
+import { useState } from 'react';
 
-import { type AppRouter } from "@/server/api/root";
-import { getUrl, transformer } from "./shared";
+import { type AppRouter } from '@/server/api/root';
+
+import { getUrl, transformer } from './shared';
 
 export const api = createTRPCReact<AppRouter>();
 
@@ -22,19 +23,19 @@ export function TRPCReactProvider(props: {
       links: [
         loggerLink({
           enabled: (op) =>
-            process.env.NODE_ENV === "development" ||
-            (op.direction === "down" && op.result instanceof Error),
+            process.env.NODE_ENV === 'development' ||
+            (op.direction === 'down' && op.result instanceof Error),
         }),
         unstable_httpBatchStreamLink({
           url: getUrl(),
           headers() {
             const heads = new Map(props.headers);
-            heads.set("x-trpc-source", "react");
+            heads.set('x-trpc-source', 'react');
             return Object.fromEntries(heads);
           },
         }),
       ],
-    })
+    }),
   );
 
   return (
