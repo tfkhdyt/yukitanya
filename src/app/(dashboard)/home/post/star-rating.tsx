21e57c4299109059fe94ee1f9c321b8fc5898716
tfkhdyt <me@tfkhdyt.my.id>
2023-11-05 12:39:39 +0700

    feat(home/post): add star rating component

---
+++ src/app/(dashboard)/home/post/star-rating.tsx
@@ -0,0 +1,28 @@
+import { Star } from 'lucide-react';
+
+export function StarRating({ rating }: { rating: number }) {
+  // Calculate the number of full stars and the number of half stars
+  const fullStars = Math.round(rating);
+  //   const hasHalfStar = rating % 1 !== 0;
+
+  // Create an array of JSX elements to represent the stars
+  const stars = [];
+  for (let i = 0; i < fullStars; i++) {
+    stars.push(<Star key={i} size={18} color='#F48C06' fill='#F48C06' />);
+  }
+
+  //   if (hasHalfStar) {
+  //     stars.push(
+  //       <StarHalf key='half' size={18} color='#F48C06' fill='#F48C06' />,
+  //     );
+  //   }
+
+  // Fill in the remaining stars with empty stars
+  while (stars.length < 5) {
+    stars.push(
+      <Star key={stars.length} size={18} color='#F48C06' fill='none' />,
+    );
+  }
+
+  return <div className='flex items-center space-x-1'>{stars}</div>;
+}
