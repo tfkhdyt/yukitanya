b675f73d83e714038c3e2518283637b08009e7e3
tfkhdyt <me@tfkhdyt.my.id>
2023-11-08 19:51:03 +0700

    feat(answers): show "Jawaban kosong" if answer is empty

--- src/app/(dashboard)/questions/[id]/page.tsx
+++ src/app/(dashboard)/questions/[id]/page.tsx
@@ -1,7 +1,48 @@
+import { Button } from '@/app/_components/ui/button';
+import { MessageCircle } from 'lucide-react';
+import Image from 'next/image';
 import { Answer } from './answer';
 import { DetailedQuestion } from './detailed-question';
 
 export default function Question() {
+  const answers = [
+    {
+      user: {
+        avatar: {
+          imageUrl: 'https://github.com/Rabiatul9.png',
+          fallback: 'RA',
+        },
+        fullName: 'Rabiatul Adawiyah',
+        username: 'Rabiatul9',
+      },
+      post: {
+        id: 'answer-123',
+        content: 'Maaf gak tau hehe',
+        date: new Date('2023-11-08T18:27:45'),
+        numberOfVotes: 10,
+        rating: 4.5,
+        isBestAnswer: true,
+      },
+    },
+    {
+      user: {
+        avatar: {
+          imageUrl: 'https://github.com/ihsanrzi.png',
+          fallback: 'MIR',
+        },
+        fullName: 'Muhammad Ihsan Rizaldi',
+        username: 'ihsanrzi',
+      },
+      post: {
+        id: 'answer-124',
+        content: 'B. sejajar dengan warga lainnya',
+        date: new Date('2023-11-08T19:18:00'),
+        numberOfVotes: 2,
+        rating: 2.0,
+        isBestAnswer: false,
+      },
+    },
+  ];
   return (
     <div>
       <DetailedQuestion
@@ -28,42 +69,32 @@ export default function Question() {
         }}
       />
       <div>
-        <Answer
-          user={{
-            avatar: {
-              imageUrl: 'https://github.com/Rabiatul9.png',
-              fallback: 'RA',
-            },
-            fullName: 'Rabiatul Adawiyah',
-            username: 'Rabiatul9',
-          }}
-          post={{
-            id: 'answer-123',
-            content: 'Maaf gak tau hehe',
-            date: new Date('2023-11-08T18:27:45'),
-            numberOfVotes: 10,
-            rating: 4.5,
-            isBestAnswer: true,
-          }}
-        />
-        <Answer
-          user={{
-            avatar: {
-              imageUrl: 'https://github.com/ihsanrzi.png',
-              fallback: 'MIR',
-            },
-            fullName: 'Muhammad Ihsan Rizaldi',
-            username: 'ihsanrzi',
-          }}
-          post={{
-            id: 'answer-124',
-            content: 'B. sejajar dengan warga lainnya',
-            date: new Date('2023-11-08T19:18:00'),
-            numberOfVotes: 2,
-            rating: 2.0,
-            isBestAnswer: false,
-          }}
-        />
+        {answers.length > 0 ? (
+          answers.map((answer) => (
+            <Answer
+              user={answer.user}
+              post={answer.post}
+              key={answer.post.id}
+            />
+          ))
+        ) : (
+          <div className='p-6'>
+            <Image
+              src='/img/questions/jawaban-kosong.png'
+              alt='Jawaban Kosong'
+              height={178}
+              width={213}
+              className='mx-auto'
+            />
+            <p className='text-center text-sm font-medium text-gray-500'>
+              Taufik Hidayat yang ganteng menunggu bantuan jawabanmu
+            </p>
+            <Button className='mx-auto mt-4 flex items-center space-x-2 rounded-full font-semibold'>
+              <MessageCircle size={16} />
+              <p>Tambahkan Jawabanmu!</p>
+            </Button>
+          </div>
+        )}
       </div>
     </div>
   );
