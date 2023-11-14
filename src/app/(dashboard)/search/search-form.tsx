afa782c00b79bf22a51c6c577a53d75bbff71eb6
tfkhdyt <me@tfkhdyt.my.id>
2023-11-14 21:03:17 +0700

    feat(search): add search post by subject featire

--- src/app/(dashboard)/search/search-form.tsx
+++ src/app/(dashboard)/search/search-form.tsx
@@ -1,10 +1,22 @@
 'use client';
 
+import { Button } from '@/app/_components/ui/button';
 import { Input } from '@/app/_components/ui/input';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/app/_components/ui/select';
+import { mapel } from '@/constants/mapel';
 import { questions } from '@/constants/question';
 import { useDebounce } from '@uidotdev/usehooks';
+import { PencilIcon } from 'lucide-react';
+import Image from 'next/image';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import { QuestionModal } from '../home/question/question-modal';
 import { QuestionPost } from '../home/question/question-post';
 
 export function SearchForm() {
@@ -12,35 +24,61 @@ export function SearchForm() {
   const router = useRouter();
 
   const q = searchParams.get('q');
+  const s = searchParams.get('s');
+
   const [query, setQuery] = useState(q ?? '');
+  const [subject, setSubject] = useState(s ?? 'all');
+
   const debouncedQuery = useDebounce(query, 500);
+  const debouncedSubject = useDebounce(subject, 500);
+
+  const filteredPost = questions
+    .filter((question) => {
+      if (subject === 'all') {
+        return true;
+      } else {
+        return question.subject.id === subject;
+      }
+    })
+    .filter((question) => {
+      const searchKeywords = debouncedQuery.toLowerCase().split(' ');
+
+      return searchKeywords.every((word) =>
+        question.content.toLowerCase().includes(word),
+      );
+    });
 
   useEffect(() => {
-    router.push(`/search?q=${debouncedQuery}`);
-  }, [debouncedQuery, router]);
+    router.push(`/search?q=${debouncedQuery}&s=${debouncedSubject}`);
+  }, [debouncedQuery, debouncedSubject, router]);
 
   return (
     <>
-      <form className='flex w-full items-center p-4'>
+      <form className='flex w-full items-center space-x-1 p-4'>
         <Input
           type='text'
           placeholder='Cari...'
-          className='rounded-full'
+          className='rounded-l-full'
           value={query}
           onChange={(e) => setQuery(e.target.value)}
         />
+        <Select value={subject} onValueChange={setSubject}>
+          <SelectTrigger className='w-44 rounded-r-full'>
+            <SelectValue placeholder='Mata Pelajaran' />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value='all'>Semua</SelectItem>
+            {mapel.map((mpl) => (
+              <SelectItem key={mpl.id} value={mpl.id}>
+                {mpl.name}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
       </form>
       <div>
-        {debouncedQuery &&
-          questions
-            .filter((question) => {
-              const searchKeywords = debouncedQuery.toLowerCase().split(' ');
-
-              return searchKeywords.every((word) =>
-                question.content.toLowerCase().includes(word),
-              );
-            })
-            .map((question) => (
+        {debouncedQuery && filteredPost.length > 0
+          ? filteredPost.map((question) => (
               <QuestionPost
                 key={question.id}
                 user={question.user}
@@ -55,7 +93,34 @@ export function SearchForm() {
                 }}
                 highlightedWords={debouncedQuery.toLowerCase().split(' ')}
               />
-            ))}
+            ))
+          : debouncedQuery && (
+              <div className='p-6'>
+                <Image
+                  src='/img/questions/jawaban-kosong.png'
+                  alt='Pertanyaan Kosong'
+                  height={178}
+                  width={213}
+                  className='mx-auto'
+                />
+                <p className='text-center text-sm font-medium text-gray-500'>
+                  Pertanyaan yang kamu cari tidak ditemukan
+                </p>
+                <QuestionModal
+                  fullName='Taufik Hidayat'
+                  username='tfkhdyt'
+                  avatar={{
+                    imageUrl: 'https://github.com/tfkhdyt.png',
+                    fallback: 'TH',
+                  }}
+                >
+                  <Button className='mx-auto mt-4 flex items-center space-x-2 rounded-full font-semibold'>
+                    <PencilIcon size={16} />
+                    <p>Tanyakan Sekarang!</p>
+                  </Button>
+                </QuestionModal>
+              </div>
+            )}
       </div>
     </>
   );
