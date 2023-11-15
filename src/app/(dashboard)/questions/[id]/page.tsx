140b91923d9cb04a67d5bd276be8d9b4d77cc03d
tfkhdyt <me@tfkhdyt.my.id>
2023-11-15 19:09:15 +0700

    feat(questions): generate metadata for custom title

--- src/app/(dashboard)/questions/[id]/page.tsx
+++ src/app/(dashboard)/questions/[id]/page.tsx
@@ -3,9 +3,21 @@ import { answers } from '@/constants/answer';
 import { questions } from '@/constants/question';
 import { MessageCircle } from 'lucide-react';
 import Image from 'next/image';
+import { P, match } from 'ts-pattern';
 import { AnswerModal } from './answer/answer-modal';
 import { AnswerPost } from './answer/answer-post';
 import { DetailedQuestion } from './detailed-question';
+
+export function generateMetadata({ params }: { params: { id: string } }) {
+  const id = params.id;
+  const question = questions.find((question) => question.id === id);
+
+  return {
+    title: `${match(question?.content.length)
+      .with(P.number.gte(25), () => question?.content.slice(0, 25) + '...')
+      .otherwise(() => question?.content)} - Yukitanya`,
+  };
+}
 
 export default function Question({ params }: { params: { id: string } }) {
   const question = questions.find((qst) => qst.id === params.id)!;
