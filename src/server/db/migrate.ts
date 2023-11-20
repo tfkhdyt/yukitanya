94f3b6565f099bccdcc3c65f9f663155acb7e417
tfkhdyt <me@tfkhdyt.my.id>
2023-11-20 15:45:36 +0700

    fix(migrate): end connection after migrate

--- src/server/db/migrate.ts
+++ src/server/db/migrate.ts
@@ -5,3 +5,4 @@ import postgres from 'postgres';
 
 const migrationClient = postgres(env.DATABASE_URL, { max: 1 });
 await migrate(drizzle(migrationClient), { migrationsFolder: './drizzle' });
+await migrationClient.end();
