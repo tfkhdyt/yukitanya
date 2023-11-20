9ee4c46935536d2f80b5b02abdca4451afdc65ac
tfkhdyt <me@tfkhdyt.my.id>
2023-11-20 21:50:15 +0700

    fix(middleware): redirect to login page if user accessing protected route without login

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,14 +8,28 @@ export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const token = await getToken({ req: request });
   const isAuthRoute = pathname.startsWith('/auth');
+  const isProtectedRoute = [
+    '/home',
+    '/favorite',
+    '/notifications',
+    '/questions',
+    '/search',
+    '/subjects',
+    '/users',
+  ].some((route) => pathname.startsWith(route));
+  console.log(token);
 
   if (isAuthRoute) {
-    if (pathname === '/auth' || pathname === '/auth/') {
-      return NextResponse.redirect(new URL('/auth/sign-in', request.url));
-    }
-
     if (token) {
       return NextResponse.redirect(new URL('/home', request.url));
     }
+
+    if (pathname === '/auth' || pathname === '/auth/') {
+      return NextResponse.redirect(new URL('/auth/sign-in', request.url));
+    }
+  }
+
+  if (isProtectedRoute && !token) {
+    return NextResponse.redirect(new URL('/auth/sign-in', request.url));
   }
 }
