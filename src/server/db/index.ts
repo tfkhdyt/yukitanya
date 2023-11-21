f0256d3c411ea649cf3f0d34f410d29637dad6c9
tfkhdyt <me@tfkhdyt.my.id>
2023-11-21 07:35:31 +0700

    fix(db): implement singleton

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,8 +1,23 @@
 import { env } from '@/env.mjs';
 import * as schema from '@/server/db/schema';
-import { drizzle } from 'drizzle-orm/postgres-js';
+import { type PostgresJsDatabase, drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
+
+declare global {
+  // eslint-disable-next-line no-var -- only var works here
+  var db: PostgresJsDatabase<typeof schema> | undefined;
+}
+
+let db: PostgresJsDatabase<typeof schema>;
 
 // for query purposes
 const queryClient = postgres(env.DATABASE_URL);
-export const db = drizzle(queryClient, { schema });
+if (env.NODE_ENV === 'production') {
+  db = drizzle(queryClient, { schema });
+} else {
+  if (!global.db) global.db = drizzle(queryClient, { schema });
+
+  db = global.db;
+}
+
+export { db };
