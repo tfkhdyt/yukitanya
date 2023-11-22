81c7c7f3b50cafb81290d69cba76ae17fa102c6d
tfkhdyt <me@tfkhdyt.my.id>
2023-11-22 09:04:51 +0700

    refactor(drizzle/migrate): use iife

--- src/server/db/migrate.ts
+++ src/server/db/migrate.ts
@@ -4,8 +4,9 @@ import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from 'postgres';
 
 const migrationClient = postgres(env.DATABASE_URL, { max: 1 });
-void migrate(drizzle(migrationClient), { migrationsFolder: './drizzle' }).then(
-  async () => {
-    await migrationClient.end();
-  },
-);
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+(async () => {
+  await migrate(drizzle(migrationClient), { migrationsFolder: './drizzle' });
+  await migrationClient.end();
+})();
