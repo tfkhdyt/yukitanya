c385e216df44ae3b9474c0f8ddc93150e32b190c
tfkhdyt <me@tfkhdyt.my.id>
2023-11-25 08:11:33 +0700

    fix(sign-up-page): increase margin bottom to prevent overlapping with footer

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -15,7 +15,7 @@ export const metadata = {
 export default function SignUp() {
   return (
     <>
-      <section className='mb-40 h-screen bg-[url(/img/hero_bg.svg)] bg-cover lg:bg-bottom'>
+      <section className='mb-64 h-screen bg-[url(/img/hero_bg.svg)] bg-cover lg:bg-bottom'>
         <div className='container px-5 pt-5 md:pt-12'>
           <Card className='mx-auto w-fit rounded-3xl border-2 border-black p-0 md:p-6'>
             <CardHeader>
