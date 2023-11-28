0884fe4e0df6120dab0be296027baf29c323378b
tfkhdyt <me@tfkhdyt.my.id>
2023-11-28 19:42:45 +0700

    perf(edit-question-modal): remove unnecessary invalidation

--- src/app/_components/modals/edit-question-modal.tsx
+++ src/app/_components/modals/edit-question-modal.tsx
@@ -90,7 +90,6 @@ export function EditQuestionModal({
       form.reset();
       await utils.question.invalidate();
       await utils.favorite.findAllFavoritedQuestions.invalidate();
-      await utils.user.findUserStatByUsername.invalidate();
     },
   });
 
