2810d6ad39c00e8eb176c6e290520e9966449adb
tfkhdyt <me@tfkhdyt.my.id>
2023-11-29 07:02:15 +0700

    Revert "fix(user-entry): move Link to user name and username"
    This reverts commit 2bb88e7c11aae05377d25ade97f465720e14c65d.

--- src/app/(dashboard)/search/user-entry.tsx
+++ src/app/(dashboard)/search/user-entry.tsx
@@ -12,24 +12,26 @@ export function UserEntry({
   };
 }) {
   return (
-    <div className='flex items-center space-x-3 border-b-2 p-4 text-[#696984] transition hover:bg-slate-50'>
-      <div>
-        <Avatar>
-          <AvatarImage src={user.image ?? getDiceBearAvatar(user.username)} />
-          <AvatarFallback>{createInitial(user.name)}</AvatarFallback>
-        </Avatar>
+    <Link href={`/users/${user.username}`}>
+      <div className='flex items-center space-x-3 border-b-2 p-4 text-[#696984] transition hover:bg-slate-50'>
+        <div>
+          <Avatar>
+            <AvatarImage src={user.image ?? getDiceBearAvatar(user.username)} />
+            <AvatarFallback>{createInitial(user.name)}</AvatarFallback>
+          </Avatar>
+        </div>
+        <div className='text-[#696984]'>
+          <p
+            className='line-clamp-1 font-medium decoration-2 hover:underline'
+            title={user.name ?? user.username}
+          >
+            {user.name}
+          </p>
+          <p className='line-clamp-1 font-normal' title={`@${user.username}`}>
+            @{user.username}
+          </p>
+        </div>
       </div>
-      <Link href={`/users/${user.username}`} className='text-[#696984]'>
-        <p
-          className='line-clamp-1 font-medium decoration-2 hover:underline'
-          title={user.name ?? user.username}
-        >
-          {user.name}
-        </p>
-        <p className='line-clamp-1 font-normal' title={`@${user.username}`}>
-          @{user.username}
-        </p>
-      </Link>
-    </div>
+    </Link>
   );
 }
