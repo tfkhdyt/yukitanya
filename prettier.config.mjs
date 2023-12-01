eb66d83dea635f4efac0291081ad2556e3a460c6
tfkhdyt <me@tfkhdyt.my.id>
2023-12-01 07:44:25 +0700

    chore: remove eslint and prettier config

--- prettier.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options} */
-const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
-  singleQuote: true,
-  jsxSingleQuote: true,
-};
-
-export default config;
