7aeb1332ec612e5d82c7377941a1117a17528f86
tfkhdyt <me@tfkhdyt.my.id>
2023-12-02 09:56:08 +0700

    feat(subjects): implement infinite scroll

--- src/app/(dashboard)/subjects/[id]/question-list.tsx
+++ src/app/(dashboard)/subjects/[id]/question-list.tsx
@@ -7,6 +7,8 @@ import { QuestionPost } from '@/components/question/question-post';
 import { SkeletonQuestionPost } from '@/components/question/skeleton-question-post';
 import { createInitial } from '@/lib/utils';
 import { api } from '@/trpc/react';
+import { useIntersectionObserver } from '@uidotdev/usehooks';
+import { useEffect } from 'react';
 
 export function QuestionList({
 	session,
@@ -15,17 +17,36 @@ export function QuestionList({
 	session: Session | null;
 	subjectId: string;
 }) {
-	const questions = api.question.findAllQuestionsBySubject.useQuery(subjectId);
+	const { isLoading, data, isError, error, fetchNextPage, isFetchingNextPage } =
+		api.question.findAllQuestionsBySubject.useInfiniteQuery(
+			{
+				subjectId,
+				limit: 10,
+			},
+			{
+				getNextPageParam: (lastPage) => lastPage.nextCursor,
+			},
+		);
 
-	if (questions.isLoading || !questions.data) {
+	const [reference, entry] = useIntersectionObserver({ threshold: 0 });
+
+	useEffect(() => {
+		if (entry?.isIntersecting) {
+			void fetchNextPage();
+		}
+	}, [entry, fetchNextPage]);
+
+	const questions = data?.pages.flatMap((page) => page.data);
+
+	if (isLoading) {
 		return <SkeletonQuestionPost />;
 	}
 
-	if (questions.isError) {
-		return <PertanyaanKosong title={questions.error?.message} />;
+	if (isError) {
+		return <PertanyaanKosong title={error?.message} />;
 	}
 
-	if (questions.data.length === 0) {
+	if (questions?.length === 0 || !questions) {
 		return (
 			<PertanyaanKosong
 				title='Pertanyaan dalam mata pelajaran ini belum tersedia'
@@ -37,7 +58,7 @@ export function QuestionList({
 
 	return (
 		<>
-			{questions.data.map((question) => {
+			{questions.map((question, index) => {
 				const bestAnswerRatings =
 					question.answers.find((answer) => answer.isBestAnswer === true)
 						?.ratings ?? [];
@@ -49,30 +70,35 @@ export function QuestionList({
 				const averageRating = totalRating / bestAnswerRatings?.length;
 
 				return (
-					<QuestionPost
+					<div
+						ref={index === questions.length - 1 ? reference : undefined}
 						key={question.id}
-						question={{
-							content: question.content,
-							createdAt: question.createdAt,
-							id: question.id,
-							isFavorited: question.favorites.some(
-								(favorite) => favorite.userId === session?.user.id,
-							),
-							numberOfAnswers: question.answers.length,
-							numberOfFavorites: question.favorites.length,
-							rating: Number.isNaN(averageRating) ? undefined : averageRating,
-							subject: question.subject,
-							updatedAt: question.updatedAt,
-							slug: question.slug,
-							owner: {
-								...question.owner,
-								initial: createInitial(question.owner.name),
-							},
-						}}
-						session={session}
-					/>
+					>
+						<QuestionPost
+							question={{
+								content: question.content,
+								createdAt: question.createdAt,
+								id: question.id,
+								isFavorited: question.favorites.some(
+									(favorite) => favorite.userId === session?.user.id,
+								),
+								numberOfAnswers: question.answers.length,
+								numberOfFavorites: question.favorites.length,
+								rating: Number.isNaN(averageRating) ? undefined : averageRating,
+								subject: question.subject,
+								updatedAt: question.updatedAt,
+								slug: question.slug,
+								owner: {
+									...question.owner,
+									initial: createInitial(question.owner.name),
+								},
+							}}
+							session={session}
+						/>
+					</div>
 				);
 			})}
+			{isFetchingNextPage && <SkeletonQuestionPost />}
 		</>
 	);
 }
