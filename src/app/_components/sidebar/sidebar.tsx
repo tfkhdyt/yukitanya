4f87f30ad5dc9501601fd0129af014f0953e9b23
tfkhdyt <me@tfkhdyt.my.id>
2023-12-03 14:08:10 +0700

    refactor(notif): create NotifCounter component

--- src/app/_components/sidebar/sidebar.tsx
+++ src/app/_components/sidebar/sidebar.tsx
@@ -9,7 +9,7 @@ import { type User } from '@/server/auth';
 import { useSidebarStore } from '@/stores/sidebar';
 
 import { menu } from '@/constants/menu';
-import { api } from '@/trpc/react';
+import { NotifCount } from '../notif-counter';
 import { ProfileButton } from './profile-button';
 
 export function Sidebar({
@@ -21,7 +21,6 @@ export function Sidebar({
 }) {
 	const pathname = usePathname();
 	const toggleSidebar = useSidebarStore((state) => state.toggle);
-	const notifCount = api.notification.getNotificationCount.useQuery(user?.id);
 
 	return (
 		<nav className='space-y-6 text-[#696984] md:p-3 lg:space-y-8 lg:p-6'>
@@ -53,13 +52,9 @@ export function Sidebar({
 									return (
 										<>
 											<each.icon size={28} strokeWidth={2} />
-											{each.url === '/notifications' &&
-												notifCount.data &&
-												notifCount.data > 0 && (
-													<div className='absolute top-0 left-1.5 inline-flex items-center justify-center px-2 py-1 text-xs font-semibold leading-none text-white bg-[#F48C06] rounded-full'>
-														{notifCount.data}
-													</div>
-												)}
+											{each.url === '/notifications' && user && (
+												<NotifCount userId={user.id} />
+											)}
 											<p className='text-xl font-medium'>{each.title}</p>
 										</>
 									);
@@ -67,13 +62,9 @@ export function Sidebar({
 								.otherwise(() => (
 									<>
 										<each.icon size={28} strokeWidth={1} />
-										{each.url === '/notifications' &&
-											notifCount.data &&
-											notifCount.data > 0 && (
-												<div className='absolute top-0 left-1.5 inline-flex items-center justify-center px-2 py-1 text-xs font-semibold leading-none text-white bg-[#F48C06] rounded-full'>
-													{notifCount.data}
-												</div>
-											)}
+										{each.url === '/notifications' && user && (
+											<NotifCount userId={user.id} />
+										)}
 										<p className='text-xl font-light'>{each.title}</p>
 									</>
 								))}
