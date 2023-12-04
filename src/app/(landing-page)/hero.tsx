a9c99dfe0b1e5beecaa390f815ed2e75ed9fc26c
tfkhdyt <me@tfkhdyt.my.id>
2023-12-04 15:38:35 +0700

    feat(landing/hero): make search feature works

--- src/app/(landing-page)/hero.tsx
+++ src/app/(landing-page)/hero.tsx
@@ -2,11 +2,20 @@
 
 import { Search } from 'lucide-react';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 export function Hero() {
+	const [query, setQuery] = useState('');
+	const router = useRouter();
+
+	const handleSearch = (q: string) => {
+		router.push(`/search?type=question&subject=all&query=${q}`);
+	};
+
 	return (
 		<section
-			className='h-auto bg-[url(/img/hero_bg.svg)] bg-cover bg-bottom md:bg-left lg:h-screen'
+			className='h-auto bg-[url(/img/hero_bg.svg)] bg-cover bg-bottom md:bg-left lg:min-h-[100svh]'
 			id='home'
 		>
 			<div className='container flex flex-wrap items-center gap-12 pb-96 pt-32 md:flex-nowrap md:px-12'>
@@ -20,11 +29,20 @@ export function Hero() {
 					<p className='text-4xl font-extrabold text-[#77425A] drop-shadow-2xl lg:text-5xl'>
 						Untuk Bertanya!
 					</p>
-					<div className='flex items-center rounded-full bg-white font-light shadow-md'>
+					<form
+						className='flex items-center rounded-full bg-white font-light shadow-md'
+						role='search'
+						onSubmit={(e) => {
+							e.preventDefault();
+							handleSearch(query);
+						}}
+					>
 						<input
 							className='w-full rounded-full px-6 text-[#77425A] placeholder:text-[#939090] focus:outline-none lg:px-8 lg:py-1 lg:text-xl'
 							placeholder='Cari pertanyaan...!'
 							type='text'
+							value={query}
+							onChange={(e) => setQuery(e.target.value)}
 						/>
 						<button
 							aria-label='Search'
@@ -33,7 +51,7 @@ export function Hero() {
 						>
 							<Search />
 						</button>
-					</div>
+					</form>
 				</div>
 				<div className='md:w-1/2'>
 					<Image
