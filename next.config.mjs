504fe00ee887ec49344fff2725daa34b573e5593
tfkhdyt <me@tfkhdyt.my.id>
2023-12-07 05:59:28 +0700

    build: setup next.js bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,15 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
+import bundleAnalyzer from '@next/bundle-analyzer';
+
 await import('./src/environment.mjs');
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true'
+})
 
 /** @type {import("next").NextConfig} */
 const config = {};
 
-export default config;
+export default withBundleAnalyzer(config);
