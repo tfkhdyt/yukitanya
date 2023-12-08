451396de4aae3c673075402c1b8996dfbea760d8
tfkhdyt <me@tfkhdyt.my.id>
2023-12-08 14:30:05 +0700

    fix(algolia/question): use empty string instead of undefined

--- src/server/api/routers/question.ts
+++ src/server/api/routers/question.ts
@@ -198,12 +198,12 @@ export const questionRouter = createTRPCRouter({
 			}),
 		)
 		.query(async ({ ctx, input }) => {
+			console.info(`subjectId: ${input.subjectId}`);
 			const searchResults = await questionIndex.search(input.query, {
 				filters:
-					input.subjectId !== 'all'
-						? `subjectId:${input.subjectId}`
-						: undefined,
+					input.subjectId !== 'all' ? `subjectId:${input.subjectId}` : '',
 			});
+			console.info('Search result:', searchResults);
 			const hitsIds = searchResults.hits.map((hit) => hit.objectID);
 			if (hitsIds.length === 0) {
 				throw new Error('Pertanyaan yang kamu cari tidak ditemukan');
