65d9243247e53d70399f4519392db87044860851
tfkhdyt <me@tfkhdyt.my.id>
2023-12-13 11:11:56 +0700

    fix(api/question): change to relational query

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -1,14 +1,18 @@
 import { db } from '@/server/db';
 import { questions } from '@/server/db/schema';
+import { desc } from 'drizzle-orm';
 
 export async function GET() {
-	const data = await db
-		.select({
-			id: questions.id,
-			content: questions.content,
-			subjectId: questions.subjectId,
-		})
-		.from(questions);
+	// const data = await db
+	// 	.select({
+	// 		id: questions.id,
+	// 		content: questions.content,
+	// 		subjectId: questions.subjectId,
+	// 	})
+	// 	.from(questions);
+	const data = await db.query.questions.findMany({
+		orderBy: desc(questions.createdAt),
+	});
 
 	return Response.json(data);
 }
