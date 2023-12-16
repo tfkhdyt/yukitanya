3bbbf9ccb7d33da1307e6d6e75820629c6ed88e4
tfkhdyt <me@tfkhdyt.my.id>
2023-12-16 17:50:24 +0700

    fix(answer-post): revalidate all question route deleting answer

--- src/app/_components/answer/answer-post.tsx
+++ src/app/_components/answer/answer-post.tsx
@@ -87,7 +87,7 @@ export function AnswerPost({
 
 			await Promise.allSettled([
 				utils.answer.invalidate(),
-				utils.question.findQuestionMetadata.invalidate(),
+				utils.question.invalidate(),
 				utils.user.invalidate(),
 			]);
 		},
