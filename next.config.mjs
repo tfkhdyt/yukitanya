ea3c5b45ee235aa98f74f807d6b49edf7425b6cc
tfkhdyt <me@tfkhdyt.my.id>
2023-12-17 07:43:30 +0700

    feat(pwa): try serwist

---
+++ next.config.mjs
@@ -0,0 +1,32 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+import withSerwistInit from '@serwist/next';
+
+await import('./src/environment.mjs');
+
+// const withBundleAnalyzer = bundleAnalyzer({
+// 	enabled: process.env.ANALYZE === 'true',
+// });
+
+const withSerwist = withSerwistInit({
+	cacheOnFrontEndNav: true,
+	swSrc: 'app/sw.ts',
+	swDest: 'public/sw.js',
+	disable: process.env.NODE_ENV === 'development',
+});
+
+/** @type {import("next").NextConfig} */
+const config = {
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				hostname: 'utfs.io',
+			},
+		],
+	},
+};
+
+export default withSerwist(config);
