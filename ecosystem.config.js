0bbd2b3c5d5bce51426b37cd9d8fc702942d2c09
tfkhdyt <tfkhdyt@proton.me>
2024-03-27 21:23:08 +0700

    build: switch next to using local installed binary
    Updates the script used to start the Next.js app to use the locally installed binary instead of the global one.
    
    This change ensures that the Next.js app is started using the locally installed version, which may have different settings or dependencies than the global version.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'yukitanya',
-      script: './node_modules/.bin/next',
+      script: 'node_modules/next/dist/bin/next',
       args: 'start -p 3003',
       exec_mode: 'cluster',
       instances: 'max',
