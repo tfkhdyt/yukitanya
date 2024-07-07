421d4daff95e7e3b1dbf5fb277e4c8221b2ae674
Taufik Hidayat <tfkhdyt@proton.me>
2024-07-07 10:38:43 +0700

    fix(most-active-users): show score in tooltip

--- src/app/_components/sidebar/most-active-users.tsx
+++ src/app/_components/sidebar/most-active-users.tsx
@@ -4,6 +4,13 @@ import Link from 'next/link';
 import { AvatarWithBadge } from '../avatar-with-badge';
 import { SkeletonMostActiveUsers } from './skeleton-most-active-users';
 
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+
 export function MostActiveUsers({
   setSheetOpen,
 }: {
@@ -24,48 +31,51 @@ export function MostActiveUsers({
 
         <ol className='space-y-5'>
           {data.map(({ user, score }, index) => (
-            <li key={user.id} className='flex space-x-2 items-center'>
-              <div className='text-[#696984] w-4 pr-2 font-medium text-lg'>
-                <span>{index + 1}.</span>
-              </div>
-              <Link
-                href={`/users/${user.username}`}
-                aria-label={user.username}
-                onClick={setSheetOpen}
-              >
-                <AvatarWithBadge
-                  user={{
-                    ...user,
-                    membership: user.membership,
-                    initial: createInitial(user.name ?? undefined),
-                  }}
-                />
-              </Link>
-              <div className='text-[#696984] grow'>
-                <Link
-                  className='max-w-[9rem] cursor-pointer truncate font-medium decoration-2 hover:underline md:max-w-[12rem] block text-base'
-                  href={`/users/${user.username}`}
-                  onClick={setSheetOpen}
-                  title={user.name ?? user.username}
-                >
-                  {user.name}
-                </Link>
-                <Link
-                  className='max-w-[10rem] truncate font-normal md:max-w-[12rem] block text-base'
-                  href={`/users/${user.username}`}
-                  onClick={setSheetOpen}
-                  title={`@${user.username}`}
-                >
-                  @{user.username}
-                </Link>
-              </div>
-              <p
-                className='ml-auto bg-orange-500 rounded-full text-white p-2 size-8 text-sm flex justify-center items-center font-medium'
-                title='Score, dihitung berdasarkan jumlah pertanyaan, jawaban, dan favorit'
-              >
-                {score}
-              </p>
-            </li>
+            <TooltipProvider key={user.id}>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <li className='flex space-x-2 items-center'>
+                    <div className='text-[#696984] w-4 pr-2 font-medium text-lg'>
+                      <span>{index + 1}.</span>
+                    </div>
+                    <Link
+                      href={`/users/${user.username}`}
+                      aria-label={user.username}
+                      onClick={setSheetOpen}
+                    >
+                      <AvatarWithBadge
+                        user={{
+                          ...user,
+                          membership: user.membership,
+                          initial: createInitial(user.name ?? undefined),
+                        }}
+                      />
+                    </Link>
+                    <div className='text-[#696984] grow'>
+                      <Link
+                        className='max-w-[10rem] cursor-pointer truncate font-medium decoration-2 hover:underline md:max-w-[12rem] block text-base'
+                        href={`/users/${user.username}`}
+                        onClick={setSheetOpen}
+                        title={user.name ?? user.username}
+                      >
+                        {user.name}
+                      </Link>
+                      <Link
+                        className='max-w-[10rem] truncate font-normal md:max-w-[12rem] block text-base'
+                        href={`/users/${user.username}`}
+                        onClick={setSheetOpen}
+                        title={`@${user.username}`}
+                      >
+                        @{user.username}
+                      </Link>
+                    </div>
+                  </li>
+                </TooltipTrigger>
+                <TooltipContent className='my-6'>
+                  <p>Score: {score}</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
           ))}
         </ol>
       </div>
